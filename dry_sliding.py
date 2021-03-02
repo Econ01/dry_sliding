7d1809dbaa5154b7cef8447b758651bde0e7337c
Econ <alicemcakmak97@gmail.com>
2021-03-02 14:07:29 +0300

    Cleaned the code

--- dry_sliding.py
+++ dry_sliding.py
@@ -19,6 +19,8 @@ subsc = 1001 #Number of substrate atoms in the system (This must be an odd numbe
 dt = 0.01 #Time interval
 Steps = 5000 #Step Count
 t = [0] #Time
+heat_total = [] #To store lost energy
+target_heat = 24 #Environment temperature in celsius
 distance_substrate = 3 #Distance between each substrate atom (In Angstrom)
 coordinates = int((subsc-1)*distance_substrate/2) #to calculate starting coordinates of the substrates
 
@@ -63,42 +65,45 @@ printProgressBar(0,Steps,prefix="Progress",suffix="Complete",length=50)
 #Lennard-Jones Potential Force Between single substrate and sliding onject
 def InteractionForce_Substrate_X(xi_temp,xt,zi_temp,zt):
     r = math.sqrt(math.pow((xt[-1]-xi_temp),2)+math.pow(zt[-1]-zi_temp,2))
-    V_ti = 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(xi_temp-xt[-1])
-    return V_ti
+    IF_Substrate_X = 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(xi_temp-xt[-1])
+    return IF_Substrate_X
 
 def InteractionForce_Substrate_Z(xi_temp,xt,zi_temp,zt):
     r = math.sqrt(math.pow((xt[-1]-xi_temp),2)+math.pow((zt[-1]-zi_temp),2))
-    V_ti_Z = 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(zi_temp-zt[-1])
-    return V_ti_Z
+    IF_Substrate_Z = 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(zi_temp-zt[-1])
+    return IF_Substrate_Z
+#--------------------------------------------------------------------------
 
 #Lennard-Jones Potential Force between closest 61 atoms and the sliding object
 def InteractionForce_Object_X(xtf,xi,ztf,zi):
-    V_ti_2 = 0
+    IF_Object_X = 0
     for l in range(range_min,range_max):
         r = math.sqrt(math.pow((xtf-xi[l]),2) + math.pow(ztf-zi[l],2))
-        V_ti_2 = V_ti_2 + 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(xtf-xi[l])
-    return V_ti_2
+        IF_Object_X = IF_Object_X + 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(xtf-xi[l])
+    return IF_Object_X
 
 def InteractionForce_Object_Z(xtf,xi,ztf,zi):
-    V_ti_2_Z = 0
+    IF_Object_Z = 0
     for l in range(range_min,range_max):
         r2 = (xtf-xi[l])*(xtf-xi[l])+(ztf-zi[l])*(ztf-zi[l])
-        V_ti_2_Z = V_ti_2_Z + 4*e*6*(r0**6)*((r2**3-(2*r0**6))/(r2**7))*(ztf-zi[l])
-    return V_ti_2_Z
+        IF_Object_Z = IF_Object_Z + 4*e*6*(r0**6)*((r2**3-(2*r0**6))/(r2**7))*(ztf-zi[l])
+    return IF_Object_Z
+#--------------------------------------------------------------------------
 
-def VLJ_Cluster_Z_I(xi,zi,xtf,ztf):
+#Calculating the z coordinate for the object that makes the net force 0 in z axis
+def Object_Z_Calculator(xi,zi,xtf,ztf):
     while(True):
-        V_ti_2_Z_I = 0
+        TempForce_Z = 0
         for l in range(range_min,range_max):
             r = math.sqrt(math.pow((xtf-xi[l]),2) + math.pow(ztf-zi[l],2))
-            V_ti_2_Z_I = V_ti_2_Z_I + 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(ztf-zi[l])
-        V_ti_2_Z_I = V_ti_2_Z_I + Fn
-        if(V_ti_2_Z_I>=0 and V_ti_2_Z_I<=0.1):
+            TempForce_Z = TempForce_Z + 4*e*(-((12*r0**12)/r**14)+((6*r0**6)/r**8))*(ztf-zi[l])
+        TempForce_Z = TempForce_Z + Fn
+        if(TempForce_Z>=0 and TempForce_Z<=0.1):
             return ztf
         else:
-            if(V_ti_2_Z_I<0):
+            if(TempForce_Z<0):
                 ztf+=0.001
-            elif(V_ti_2_Z_I>0):
+            elif(TempForce_Z>0):
                 ztf-=0.001
 
 def Heat(range_min,range_max,vi):
@@ -107,6 +112,12 @@ def Heat(range_min,range_max,vi):
         total_energy = total_energy + (0.5)*m*(vi[n]**2)
     heat = (total_energy / (1.5*kB*(range_max-range_min))) * ((math.pow(10,-24))/(6.022))
     return heat
+
+#Rescaling the velocities according to the normalized heat
+def Rescale(vi,vi_z,heat_total):
+    vi_rescale = vi*math.sqrt(target_heat/heat_total)
+    vi_z_rescale = vi_z*math.sqrt(target_heat/heat_total)
+    return vi_rescale,vi_z_rescale
 
 for l in range(Steps):
     #time
@@ -120,6 +131,7 @@ for l in range(Steps):
     range_min = int(((subsc-1)/2)+(xt_a))
     range_max = int(((subsc-1)/2)+61+(xt_a))
 
+    #Calculation of velocities and next coordinates of substrates
     for n in range(range_min,range_max):
         zi_temp = zi_temporary[n]
         xi_temp = xi_temporary[n]
@@ -127,11 +139,11 @@ for l in range(Steps):
         vi_z[n] = (vi_z[n]-(((kz*(zi[n]-zi_0[n])+InteractionForce_Substrate_Z(xi_temp,xt,zi_temp,zt)+0.1*vi_z[n])*dt)/m)*converter)
         zi[n] = vi_z[n]*dt + zi[n]
         xi[n] = vi[n]*dt + xi[n]
+    #--------------------------------------------------------------------------
 
-    heat = Heat(range_min,range_max,vi)
-    #print(heat)
+    heat_total.append(Heat(range_min,range_max,vi))
 
-    Ff.append(kt*(xd[0]-xd[-1]+xt[-1])) #calculation of friction force with damping
+    Ff.append(kt*(xd[0]-xd[-1]+xt[-1])) #calculation of friction force
 
     xd.append(vD*dt+xd[-1]) #calculation of the next position of the moving agent
 
@@ -139,14 +151,15 @@ for l in range(Steps):
     xtf = xt[-1]
     ztf = zt[-1]
 
+    #Calculation of velocities and next coordinates of the object
     vt.append(vt[-1]-((InteractionForce_Object_X(xtf,xi,ztf,zi)+(Ff[-1])+0.1*vt[-1])*dt/m)*converter)
     xt.append(xt[l]+vt[-1]*dt)
 
-    z_true = VLJ_Cluster_Z_I(xi,zi,xtf,ztf)
+    z_true = Object_Z_Calculator(xi,zi,xtf,ztf)
 
     vt_z.append(vt_z[-1]-((Fn + InteractionForce_Object_Z(xtf,xi,ztf,zi) + 4.3*(zt[-1]-(z_true)) + 0.1*vt_z[-1])*dt/m)*converter)
     zt.append(zt[l]+vt_z[-1]*dt)
-    #zt.append(z_true)
+    #--------------------------------------------------------------------------
 
     Fid1.write("{0:d}\n\n".format(subsc+2))
     for i in range(len(xi)):
